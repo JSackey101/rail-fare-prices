8b23cdfeb49f9e1b8493ae7931fe6dc5208300ab
JSackey101 <jeffsackey101@gmail.com>
2023-11-14 23:38:22 +0000

    Fixed an issue with the read_rail_network function where all the station objects created from the file given were treated as hub stations when that was not the case.

--- utilities.py
+++ utilities.py
@@ -8,10 +8,10 @@ def read_rail_network(filepath):
     latitude, longitude and whether it is a hub station, reads the data within the file, uses the data to create one
     or more station objects and returns a rail network object created using the station objects created previously.
     """
-    with open(filepath, "r", newline="") as uk_stations: # Opens the file the filepath parameter leads to in read
+    with open(filepath, "r", newline="") as uk_stations:  # Opens the file the filepath parameter leads to in read
         # mode as a file object in the uk_stations variable and the newline parameter overwrites the newline escape
         # characters from the file
-        uk_stations_csv = csv.reader(uk_stations) # Creates a csv reader object from the file object
+        uk_stations_csv = csv.reader(uk_stations)  # Creates a csv reader object from the file object
         stations_data = [row for row in uk_stations_csv]  # Goes through every line in the csv reader object and adds
         # them to a nested list in the stations_data nested list
         header_info = stations_data[0]  # Saves the header list - as this would always be the first row
@@ -21,9 +21,15 @@ def read_rail_network(filepath):
         # and adds each station object to the list_of_stations list.
         list_of_stations = []
         for row in stations_data:
+            if row[header_info.index("hub")] == "1":
+                hub_station = True
+            elif row[header_info.index("hub")] == "0":
+                hub_station = False
             station = Station(row[header_info.index("name")], row[header_info.index("region")],
                               row[header_info.index("crs")], float(row[header_info.index("latitude")]),
-                              float(row[header_info.index("longitude")]), bool(row[header_info.index("hub")]))
+                              float(row[header_info.index("longitude")]), hub_station)
             list_of_stations.append(station)
-        rail_network = RailNetwork(list_of_stations) # Creates a rail network from the list of station objects
+        rail_network = RailNetwork(list_of_stations)  # Creates a rail network from the list of station objects
     return rail_network
+
+
